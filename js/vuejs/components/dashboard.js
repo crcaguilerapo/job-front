00138d92a25c0ef6c14ac5deae00cf685b234204
cristian <crcaguilerapo@unal.edu.co>
2020-08-18 18:39:31 -0500

    finish

--- js/vuejs/components/dashboard.js
+++ js/vuejs/components/dashboard.js
@@ -1,25 +1,42 @@
 const Dashboard = {
 
     mounted() {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line',
-
-            // The data for our dataset
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [{
-                    label: 'My First dataset',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
-                }]
-            },
-
-            // Configuration options go here
-            options: {}
-        });
+        let ctx = document.getElementById('myChart').getContext('2d');
+        axios
+        .get('http://127.0.0.1:8000/api/chart')
+        .then(function (response) {
+            let labels = [];
+            let values = [];
+            response.data.forEach(element => {
+                labels.push(element.name);
+                values.push(element.number);
+            });
+            console.log(labels);
+            console.log(values);
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                  labels: labels,
+                    datasets: [
+                            {
+                                label: '# de estudiantes',
+                                data: values
+                            }
+                    ]
+                },
+                options: {
+                    // All of my other bar chart option here
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+           
+        })
     },
     template: `
         <canvas id="myChart"></canvas>
--- js/vuejs/components/form.js
+++ js/vuejs/components/form.js
@@ -2,17 +2,18 @@ const Form = {
 
     data() {
         return {
-            genero: '',
-            ethnicity: '',
-            studying: '',
-            level: '',
-            faculty: '',
-            course: '',
-            graduation: '',
-            find_work: '',
-            employed: '',
-            salary: '',
-            study_work: ''
+
+
+            gender: "",
+            ethnicity: "",
+            is_studying: "",
+            level_id: "",
+            faculty_id: "",
+            graduation: "",
+            is_employed: "",
+            salary: 0,
+            contract_type: "",
+            is_work_related_to_study: ""
         }
 
 
@@ -21,21 +22,35 @@ const Form = {
     methods: {
         send: function () {
 
-            /*
-            axios.post('/user', {
-                console.log("hello world");
+            let data = {
+                gender: this.gender,
+                ethnicity: this.ethnicity,
+                is_studying: this.is_studying,
+                level_id: parseInt(this.level_id),
+                faculty_id: parseInt(this.faculty_id),
+                graduation: this.graduation,
+                is_employed: this.is_employed,
+                salary: this.salary,
+                contract_type: this.contract_type,
+                is_work_related_to_study: this.is_work_related_to_study
+            };
+            console.log(data);
+            axios.post('http://127.0.0.1:8000/api/profile', data)
+            .then(function (response) {
+                console.log(response);
                 iziToast.success({
                     title: 'Todo bien',
                     message: 'Actualización exitosa',
                 });
             })
-            .then(function (response) {
-                console.log(response);
-            })
             .catch(function (error) {
+                iziToast.error({
+                    title: 'Error',
+                    message: 'Hubo un problema',
+                });
                 console.log(error);
             });
-            */
+
         }
     },
 
@@ -43,13 +58,14 @@ const Form = {
     <div  class="own-form">
         <form v-on:submit.prevent="send">
             <div class="form-group">
-                <label for="genero">Género</label>
-                <select v-model="genero" class="form-control" id="genero" required>
+                <label for="gender">Género</label>
+                <select v-model="gender" class="form-control" id="gender" required>
                     <option>Masculino</option>
                     <option>Femenino</option>
                     <option>No binario</option>
                 </select>
             </div>
+            
             <div class="form-group">
                 <label for="ethnicity">¿Con cual minoría étnica te identificas?</label>
                 <select v-model="ethnicity" class="form-control" id="ethnicity" required>
@@ -59,54 +75,34 @@ const Form = {
                 </select>
             </div>
             <div class="form-group">
-                <label for="studying">¿Estas actualmente estudiando?</label>
-                <select v-model="studying" class="form-control" id="studying" required>
+                <label for="is_studying">¿Estas actualmente estudiando?</label>
+                <select v-model="is_studying" class="form-control" id="is_studying" required>
                     <option>Sí</option>
                     <option>No</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="level">Nivel academico</label>
-                <select v-model="level" class="form-control" id="level" required>
-                    <option>Pregrado</option>
-                    <option>Posgrado</option>
-                    <option>Doctorado</option>
+                <label for="level_id">Nivel academico</label>
+                <select v-model="level_id" class="form-control" id="level_id" required>
+                    <option value="1">Pregrado</option>
+                    <option  value="2">Posgrado</option>
+                    <option value="3">Doctorado</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="faculty">Facultad</label>
-                <select v-model="faculty" class="form-control" id="faculty" required>
-                    <option>Pregrado</option>
-                    <option>Posgrado</option>
-                    <option>Doctorado</option>
+                <label for="faculty_id">Facultad</label>
+                <select v-model="faculty_id" class="form-control" id="faculty_id" required>
+                    <option value="1">Artes</option>
+                    <option value="2">Ciencias</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="course">Carrera</label>
-                <select v-model="course" class="form-control" id="course" required>
-                    <option>Pregrado</option>
-                    <option>Posgrado</option>
-                    <option>Doctorado</option>
-                </select>
+                <label for="graduation">¿En que año se gradua?</label>
+                <input v-model="graduation" type="date" class="form-control" id="graduation" required>
             </div>
             <div class="form-group">
-                <label for="graduation">Año de graduación</label>
-                <select v-model="graduation" class="form-control" id="graduation" required>
-                    <option>Pregrado</option>
-                    <option>Posgrado</option>
-                    <option>Doctorado</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="find_work">¿Se te ha dificultado encontrar empleo?</label>
-                <select v-model="find_work" class="form-control" id="work" required>
-                    <option>SÍ</option>
-                    <option>No</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="employed">¿Estas actualmente empleado?</label>
-                <select v-model="employed" class="form-control" id="employed" required>
+                <label for="is_employed">¿Estas actualmente empleado?</label>
+                <select v-model="is_employed" class="form-control" id="is_employed" required>
                     <option>Si</option>
                     <option>No</option>
                 </select>
@@ -114,10 +110,18 @@ const Form = {
             <div class="form-group">
                 <label for="salary">¿Cuánto ganas?</label>
                 <input v-model="salary" type="range" min="0" max="100000000" step="10000" class="form-control-range" id="salary" required>
+                {{new Intl.NumberFormat().format(salary)}}
+            </div>
+            <div class="form-group">
+                <label for="contract_type">¿Se te ha dificultado encontrar empleo?</label>
+                <select v-model="contract_type" class="form-control" id="contract_type" required>
+                    <option>SÍ</option>
+                    <option>No</option>
+                </select>
             </div>
             <div id="own-last" class="form-group">
-                <label for="study_work">¿Consideras que tu actual trabajo esta relacionado directamente con tu formación academica?</label>
-                <select v-model="study_work" class="form-control" id="study_work" required>
+                <label for="is_work_related_to_study">¿Consideras que tu actual trabajo esta relacionado directamente con tu formación academica?</label>
+                <select v-model="is_work_related_to_study" class="form-control" id="is_work_related_to_study" required>
                     <option>Si</option>
                     <option>No</option>
                 </select>
